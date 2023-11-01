04303908986f965417fe878161c884ce845b0319
Sazzad Hossain <tanimsazzad@gmail.com>
2023-11-01 11:35:08 +0600

    added getDictionaryImageUrl

--- contextMenus.js
+++ contextMenus.js
@@ -3,7 +3,7 @@ let selection=''
 chrome.contextMenus.onClicked.addListener(genericOnClick);
 
 // A generic onclick callback function.
-function genericOnClick(info) {
+async function genericOnClick(info) {
   selection=info.selectionText
   switch (info.menuItemId) {
     case 'radio':
@@ -15,15 +15,15 @@ function genericOnClick(info) {
       console.log('Checkbox item clicked. Status:', info.checked);
       break;
     default:
-      // Standard context menu item function
-      console.log(`Translate "${selection}" clicked`);
+      // const data=await fetchDataFromOtherSite(selection)
+      // console.dir({data})
+      console.log(`Translate "${data}" clicked`);
   }
 }
 
 
 chrome.runtime.onInstalled.addListener(function (props) {
   // Create one test item for each context type.
-
     let title = "Translate to BANGLA";
     chrome.contextMenus.create({
       title: title,
@@ -33,5 +33,17 @@ chrome.runtime.onInstalled.addListener(function (props) {
   
   console.log({props})
 
- 
 });
+
+async function fetchDataFromOtherSite(word){
+  const res=await fetch(getDictionaryImageUrl(word),{mode:'no-cors'})
+  const data= await res.blob()
+ return data
+} 
+
+
+function getDictionaryImageUrl(word){
+  const processedWord=word.toLowerCase().trim()
+  const firstLetterOfTheWord=processedWord.charAt(0)
+  return `https://www.english-bangla.com/public/images/words/D${firstLetterOfTheWord}/${processedWord}`
+}
